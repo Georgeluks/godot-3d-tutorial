3aa80885cea8e64600155bf8ea480a6ee934d316
georgelp <george.lucasebertz98@gmail.com>
2026-01-18 17:27:53 -0300

    first commmit

---
+++ scenes/player.gd
@@ -0,0 +1,59 @@
+extends CharacterBody3D
+
+# how fast the character moves in meters per second
+@export var speed = 14
+# the downward acceleration when in the air, in meters per second squared
+@export var fall_acceleration = 75
+
+@export var jump_impulse = 20
+@export var bounce_impulse = 16
+
+var target_velocity = Vector3.ZERO
+
+func _physics_process(delta):
+	var direction = Vector3.ZERO
+	
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+	if Input.is_action_pressed("move_back"):
+		direction.z += 1
+	
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		$Pivot.basis = Basis.looking_at(direction)
+		
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
+	
+	if not is_on_floor():
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
+	
+	if is_on_floor() and Input.is_action_pressed("jump"):
+		target_velocity.y = jump_impulse
+	
+	for index in range(get_slide_collision_count()):
+		var collision = get_slide_collision(index)
+		
+		# If there are duplicate collisions with a mob in a single frame
+		# the mob will be deleted after the first collision, and a second call to
+		# get_collider will return null, leading to a null pointer when calling
+		# collision.get_collider().is_in_group("mob").
+		# This block of code prevents processing duplicate collisions.
+		if collision.get_collider() == null:
+			continue
+		
+		#if the collider is with a mob
+		if collision.get_collider().is_in_group("mob"):
+			var mob = collision.get_collider()
+			
+			if Vector3.UP.dot(collision.get_normal()) > 0.1:
+				mob.squash()
+				target_velocity.y = bounce_impulse
+				break
+		
+	velocity = target_velocity
+	move_and_slide()
